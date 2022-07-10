1ee22692f7d1b95785ce9acf1c023183a4782181
Roman Bielte <romanbielte@coherentsolutions.com>
2022-07-10 18:57:29 +0300

    Adds ProductDetailsComponent

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -6,12 +6,7 @@ import { Product } from '../products';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
-
-  @Input() product!: Product;
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
-import { products } from '../products';
+import { Component } from "@angular/core";
+import { products } from "../products";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"],
 })
 export class ProductListComponent {
   products = products;
 
+  onNotify() {
+    alert("You will be notified when the product goes on sale");
+  }
+
   share() {
-    window.alert('The product has been shared!');
+    window.alert("The product has been shared!");
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
