1c58f9b09782359f13bd5089b328dabfaca5badc
Roman Bielte <romanbielte@coherentsolutions.com>
2022-07-10 19:24:30 +0300

    Adds CartComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
 import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { CartComponent } from "./cart/cart.component";
 
 @NgModule({
   imports: [
@@ -15,14 +16,16 @@ import { ProductDetailsComponent } from "./product-details/product-details.compo
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
       { path: "products/:productId", component: ProductDetailsComponent },
+      { path: "cart", component: CartComponent },
     ]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent, 
+    ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
+    CartComponent,
   ],
   bootstrap: [AppComponent],
 })
